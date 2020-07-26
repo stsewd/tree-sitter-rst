843ca0a83971d207eb8906037bfae00c924eed9b
Santos Gallegos <stsewd@protonmail.com>
2020-07-26 09:08:51 -0500

    Add scopes queries

--- queries/locals.scm
+++ queries/locals.scm
@@ -1,3 +1,31 @@
-; Scopes
+;; Scopes
 
 (document) @local.scope
+
+(directive) @local.scope
+
+;; Definitions
+
+(substitution_definition
+  name: (substitution) @local.definition)
+
+(footnote
+  name: (label) @local.definition)
+
+(citation
+  name: (label) @local.definition)
+
+(target
+  (reference) @local.definition)
+
+; Inline targets
+(target) @local.definition
+
+;; References
+
+[
+ (substitution_reference)
+ (footnote_reference)
+ (citation_reference)
+ (reference)
+] @local.reference
