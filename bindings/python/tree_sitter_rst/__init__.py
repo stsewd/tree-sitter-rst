db4d99461a3ddf82bf768ffcb187dfa9f30da598
Justus Magin <keewis@posteo.de>
2025-01-20 16:24:09 +0100

    create bindings for python

---
+++ bindings/python/tree_sitter_rst/__init__.py
@@ -0,0 +1,7 @@
+"""Python grammar for tree-sitter"""
+
+from importlib.resources import files as _files
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ bindings/python/tree_sitter_rst/__init__.pyi
@@ -0,0 +1,3 @@
+from typing import Final
+
+def language() -> object: ...
