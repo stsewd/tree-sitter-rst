b3dca3ffa659afc5a3d803b9d7decf89c5f510e7
Santos Gallegos <stsewd@proton.me>
2025-02-02 18:16:48 -0500

    Remove trailing slash

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ $(error Windows is not supported)
 endif
 
 LANGUAGE_NAME := tree-sitter-rst
-HOMEPAGE_URL := https://github.com/stsewd/tree-sitter-rst/
+HOMEPAGE_URL := https://github.com/stsewd/tree-sitter-rst
 VERSION := 0.1.0
 
 # repository
--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-	tree_sitter_rst "github.com/stsewd/tree-sitter-rst//bindings/go"
+	tree_sitter_rst "github.com/stsewd/tree-sitter-rst/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
